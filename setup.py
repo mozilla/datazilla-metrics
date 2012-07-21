316ffa8e1e2c7af9eda69bb7042f28401e06b3cb
Carl Meyer <carl@oddbird.net>
2012-07-20 21:17:38 -0600

    Initial import; no tests, not sure welchs_ttest is working right.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name="datazilla-metrics",
+    version="0.1",
+    description="Test metrics for Datazilla",
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        ],
+    keywords="datazilla metrics",
+    author="Carl Meyer",
+    author_email="cmeyer@mozilla.com",
+    url="https://github.com/mozilla/datazilla-metrics",
+    license="BSD",
+    packages=["dzmetrics"],
+    install_requires=["scipy"],
+    zip_safe=False,
+    )
