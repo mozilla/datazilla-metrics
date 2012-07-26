8894e187c5caa320eaf198fd52c40f8c07b24c3b
Carl Meyer <carl@oddbird.net>
2012-07-26 12:13:32 -0600

    Update to latest code from metrics.

--- dzmetrics/ttest.py
+++ dzmetrics/ttest.py
@@ -53,16 +53,15 @@ def welchs_ttest(x1, x2, alpha=None):
     key ``h0_rejected`` which is ``True`` if the p value is less than alpha
     (null hypothesis rejected), otherwise ``False``.
 
-    Null hypothesis is that means are equal or mean of x2 is less than mean of
-    x1. Null hypothesis is rejected if mean of x2 is greater than mean of x1
+    Null hypothesis is that means are equal or mean of x1 is less than mean of
+    x2. Null hypothesis is rejected if mean of x1 is greater than mean of x2
     (i.e., a performance regression if the supplied data are performance test
     timings).
 
-    Code by Joseph Kelly, Mozilla metrics.
+    Original code by Joseph Kelly, Mozilla metrics.
 
     For more on Welch's t-test, see:
     http://en.wikipedia.org/wiki/Student%27s_t-test#Unequal_sample_sizes.2C_unequal_variance
-
 
     """
     n1 = len(x1)
@@ -71,13 +70,16 @@ def welchs_ttest(x1, x2, alpha=None):
     m1 = mean(x1)
     m2 = mean(x2)
 
-    s1 = std(x1)
-    s2 = std(x2)
+    s1 = std(x1, ddof=1)
+    s2 = std(x2, ddof=1)
 
-    spooled        = sqrt( power(s1,2)/n1 + power(s2,2)/n2)
+    v1             = power(s1, 2)
+    v2             = power(s2, 2)
+    vpooled        = v1/n1 + v2/n2
+    spooled        = sqrt(vpooled)
     tt             = (m1-m2)/spooled
-    df_numerator   = power( power(s1,2)/n1 + power(s2,2)/n2 , 2 )
-    df_denominator = power( power(s1,2)/n1 ,2)/(n1-1) + power( power(s2,2)/n2 ,2)/(n2-1)
+    df_numerator   = power(vpooled, 2)
+    df_denominator = power(v1/n1, 2)/(n1-1) + power(v2/n2, 2)/(n2-1)
     df             = df_numerator / df_denominator
 
     t_distribution = t(df)
